a5ed90857ff4272c68d3bab8d00c5a4a0e07b7cd
bagasy <bautielcrack4@gmail.com>
2026-07-31 13:54:35 -0300

    render(v89mlm0ixyno): deterministic composition

--- src/index_v89mlm0ixyno.tsx
+++ src/index_v89mlm0ixyno.tsx
@@ -5,7 +5,7 @@ import {BagasyTimeline_v89mlm0ixyno} from "./VideoEdit/Main_v89mlm0ixyno";
 const Root: React.FC = () => <Composition
   id="Bagasy-v89mlm0ixyno"
   component={BagasyTimeline_v89mlm0ixyno}
-  durationInFrames={49685}
+  durationInFrames={48902}
   fps={30}
   width={1920}
   height={1080}
