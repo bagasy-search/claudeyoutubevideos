c1566bdc536b4a4524309eb8ad34b9d7f5e17efe
BagasyStudio <bagasystudio@gmail.com>
2026-08-01 15:23:01 -0300

    Build premium timeline for vivuhaufdg4q

---
+++ src/index_vivuhaufdg4q.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Composition, registerRoot} from "remotion";
+import {BagasyTimeline_vivuhaufdg4q} from "./VideoEdit/Main_vivuhaufdg4q";
+
+const Root: React.FC = () => <Composition
+  id="Bagasy-vivuhaufdg4q"
+  component={BagasyTimeline_vivuhaufdg4q}
+  durationInFrames={44721}
+  fps={30}
+  width={1920}
+  height={1080}
+/>;
+registerRoot(Root);
