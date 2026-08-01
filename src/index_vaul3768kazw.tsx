fadad79b601ed7323224e437c4f6531693ecedfa
BagasyStudio <bagasystudio@gmail.com>
2026-08-01 15:10:53 -0300

    Read linaza duration from recovered timeline

--- src/index_vaul3768kazw.tsx
+++ src/index_vaul3768kazw.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Composition, registerRoot} from "remotion";
 import {BagasyTimeline_vaul3768kazw} from "./VideoEdit/Main_vaul3768kazw";
+import timeline from "./VideoEdit/timeline_vaul3768kazw.json";
 
 const Root: React.FC = () => <Composition
   id="Bagasy-vaul3768kazw"
   component={BagasyTimeline_vaul3768kazw}
-  durationInFrames={43744}
+  durationInFrames={timeline.duration_in_frames}
   fps={30}
   width={1920}
   height={1080}
